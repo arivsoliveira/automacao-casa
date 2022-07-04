50fd5b9662638f75783b2da65800c5aaae2eb2f2
Ari <ariveronica48@gmail.com>
2022-07-04 17:02:45 -0300

    testando

--- cypress/e2e/3-pesquisa-casa-nova/pesquisa-guarda-roupa.cy.js
+++ cypress/e2e/3-pesquisa-casa-nova/pesquisa-guarda-roupa.cy.js
@@ -14,3 +14,4 @@ describe('buscar guarda roupa casa nova', () => {
         cy.get('.MUFPAc > :nth-child(2) > a').click()
         cy.get(':nth-child(8) > .RhVG3d > :nth-child(1) > .sh-dr__short > .DON5yf > .EQ4p8c > .vjtvke > .JcqPK').click()    })
 })
+jdsdjoifjsdifjdsofjsdijf
